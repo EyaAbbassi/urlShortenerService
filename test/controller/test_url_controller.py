b95cb98ad7be03a8d53d53e891ef6aec727f3533
EyaAbbassi <eya.abbassi@yahoo.com>
2022-12-09 04:44:00 +0100

    Decode endpoint unittests.

--- test/controller/test_url_controller.py
+++ test/controller/test_url_controller.py
@@ -13,7 +13,8 @@ class testUrlRepository(unittest.TestCase):
 
     def setUp(self):  # run before each test case ###
         print("setUp\n")
-        self.url = ("http://127.0.0.1:5000/encode")
+        self.encodeUrl = ("http://127.0.0.1:5000/encode")
+        self.decodeUrl = ("http://127.0.0.1:5000/decode")
 
     def tearDown(self):  # run after each test case ###
         print("tearDown\n")
@@ -21,7 +22,7 @@ class testUrlRepository(unittest.TestCase):
     def test_encodeAPI_with_valid_data_should_return_OK(self):
         print("test_encodeAPI_with_valid_data_should_return_OK\n")
         data = {"long_url": "https://facebook.com/test", "custom_id": ""}
-        request = requests.post(self.url, json=data)
+        request = requests.post(self.encodeUrl, json=data)
         self.assertEqual((request.status_code, request.reason), (200, "OK"))
 
     def test_encodeAPI_with_valid_data_and_custom_key_should_return_OK(self):
@@ -29,14 +30,14 @@ class testUrlRepository(unittest.TestCase):
         # correct input with custom key
         data = {"long_url": "https://www.google.com/docs",
                 "custom_id": "YH86PEK1"}
-        request = requests.post(self.url, json=data)
+        request = requests.post(self.encodeUrl, json=data)
         self.assertEqual((request.status_code, request.reason), (200, "OK"))
 
     def test_encodeAPI_with_empty_url_should_return_BAD_REQUEST(self):
         print("test_encodeAPI_with_empty_url_should_return_BAD_REQUEST\n")
         # empty url
         data = {"long_url": "", "custom_id": ""}
-        request = requests.post(self.url, json=data)
+        request = requests.post(self.encodeUrl, json=data)
         self.assertEqual(
             (request.status_code, request.reason), (400, 'BAD REQUEST'))
 
@@ -44,7 +45,7 @@ class testUrlRepository(unittest.TestCase):
         print("test_encodeAPI_with_unvalid_url_should_return_BAD_REQUEST\n")
         # unvalid url
         data = {"long_url": "http://this/is/invalid", "custom_id": ""}
-        request = requests.post(self.url, json=data)
+        request = requests.post(self.encodeUrl, json=data)
         self.assertEqual(
             (request.status_code, request.reason), (400, 'BAD REQUEST'))
 
@@ -52,17 +53,46 @@ class testUrlRepository(unittest.TestCase):
         print("test_encodeAPI_with_existing_url_should_return_OK\n")
         # already existing url with custom key => should be updated
         data = {"long_url": "https://www.google.com/docs", "custom_id": ""}
-        request = requests.post(self.url, json=data)
+        request = requests.post(self.encodeUrl, json=data)
         self.assertEqual(
             (request.status_code, request.reason), (200, "OK"))
 
     def test_encodeAPI_with_existing_url_and_added_key_should_be_update_then_return_OK(self):
         print("test_encodeAPI_with_existing_url_and_added_key_should_be_update_then_return_OK\n")
         data = {"long_url": "https://www.google.com/docs",
-                "custom_id": "upda7e6"}
-        request = requests.post(self.url, json=data)
+                "custom_id": "Updat6e"}
+        request = requests.post(self.encodeUrl, json=data)
         self.assertEqual(
             (request.status_code, request.reason), (200, "OK"))
+
+    def test_decodeAPI_with_existing_short_url_should_return_OK(self):
+        print("test_decodeAPI_with_valid_data_should_return_OK\n")
+        data = {"short_url": "http://short.ner/YH86PEK1"}
+        request = requests.get(self.decodeUrl, json=data)
+        self.assertEqual((request.status_code, request.reason), (200, "OK"))
+
+    def test_decodeAPI_with_non_existing_short_url_should_return_NOT_FOUND(self):
+        print("test_decodeAPI_with_non_existing_short_url_should_return_NOT_FOUND\n")
+        data = {"short_url": "http://short.ner/notFound"}
+        request = requests.get(self.decodeUrl, json=data)
+        self.assertEqual((request.status_code, request.reason),
+                         (404, "NOT FOUND"))
+
+    def test_decodeAPI_with_unvalid_short_url_should_return_BAD_REQUEST(self):
+        print("test_decodeAPI_with_unvalid_short_url_should_return_BAD_REQUEST\n")
+        # correct input with custom key
+        data = {"short_url": "http://short.ner/"}
+        request = requests.get(self.decodeUrl, json=data)
+        self.assertEqual((request.status_code, request.reason),
+                         (400, "BAD REQUEST"))
+
+    def test_decodeAPI_with_empty_url_should_return_BAD_REQUEST(self):
+        print("test_decodeAPI_with_empty_url_should_return_BAD_REQUEST\n")
+        # empty url
+        data = {"short_url": ""}
+        request = requests.get(self.decodeUrl, json=data)
+        self.assertEqual(
+            (request.status_code, request.reason), (400, 'BAD REQUEST'))
 
 
 if __name__ == '__main__':
